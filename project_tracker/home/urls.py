9ec05c8501d9c8358972705d8a451dc277d4bfb5
Rohan Andharai <rohanandharia@gmail.com>
2022-01-27 10:44:36 +0530

    base html

--- project_tracker/home/urls.py
+++ project_tracker/home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path,include
 from home import views
 urlpatterns = [
     path('', views.index),
+    path('base/', views.base),
 ]
--- project_tracker/home/views.py
+++ project_tracker/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,HttpResponse
 
 def index(request):
-    return render (request,'base.html')
+    return render (request,'index.html')
 # Create your views here.
+def base(request):
+    return render (request,'base.html')
