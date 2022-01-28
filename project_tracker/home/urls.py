318b1ad756ff41782580341f6184884e1e582886
Smitesh Sorathiya <smiteshsorathiya@gmail.com>
2022-01-28 14:46:11 +0530

    Login Added

--- project_tracker/home/urls.py
+++ project_tracker/home/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path,include
 from home import views
 urlpatterns = [
     path('', views.index),
+    url(r'^login/$',views.login,name='login'),
+    url(r'^admin/$',views.admin,name='admin'),
+    url(r'^customer/$',views.customer,name='customer'),
 ]
--- project_tracker/home/views.py
+++ project_tracker/home/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render,HttpResponse
 def index(request):
     return render (request,'index.html')
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if (username=="admin" and password=="admin01"):
+            return render(request,'admin.html')
+        elif(username=="customer" and password=="customer01"):
+            return render(request,'customer.html')
+        elif(username=="#" or password=="#"):
+            print("Rooro")
+
+    return render(request,'login.html')
+
+def admin(request):
+    return render(request,'admin.html')
+
+def customer(request):
+    return render(request,'customer,html')
