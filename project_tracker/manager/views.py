18bd1b7e9f5f6c40c1fdabf24ecdcd4132e88e16
Smitesh Sorathiya <smiteshsorathiya@gmail.com>
2022-02-02 12:11:28 +0530

    responsive navbar for admin added

--- project_tracker/manager/views.py
+++ project_tracker/manager/views.py
@@ -4,15 +4,15 @@ from home.models import Client
 
 def manager(request):
     if request.user.is_authenticated and request.user.username=='Rohan2781':
-        data = Client.objects.all()
-        display=request.POST.get('disp')
-        if display=='1':
-            return redirect('/sign-up',{'data':data})
-        if display=='2':
-            return redirect('/manager/client',{'data':data})
-        if display=='3':
-            return redirect('/project/new_project',{'data':data})
-        return render(request,'admin.html',{'display':display,'data':data})
+#        data = Client.objects.all()
+#        display=request.POST.get('disp')
+#        if display=='1':
+#            return redirect('/sign-up',{'data':data})
+#        if display=='2':
+#            return redirect('/manager/client',{'data':data})
+#        if display=='3':
+#            return redirect('/project/new_project',{'data':data})
+        return render(request,'admin.html')
 # Create your views here.
 def admin_client(request):
     client = Client.objects.all()
